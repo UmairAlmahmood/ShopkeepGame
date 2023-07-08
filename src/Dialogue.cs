90bff62083534d118a168bf9cd6aea214c23c9c1
Umair Almahmood <umair.almahmood10@gmail.com>
2023-07-08 10:21:14 -0400

    typo fixes :)

--- src/Dialogue.cs
+++ src/Dialogue.cs
@@ -6,8 +6,8 @@ public static class Dialogue {
     public static List<String> greetings = new List<String>{"Hello there", "I'm looking for powerful objects", "I've heard that you're the best seller for the budget", "Hi"};
 }
 
-public struct DialogeTexts {
-    public DialogeTexts(String text, Personality personality, PlayerClass playerClass) {
+public struct DialogueTexts {
+    public DialogueTexts(String text, Personality personality, PlayerClass playerClass) {
         this.text = text;
         this.personality = personality;
         this.playerClass = playerClass;
