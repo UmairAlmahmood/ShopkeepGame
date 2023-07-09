8a2a21fadddf7c6e74dcb4ea36724187adde0084
Umair Almahmood <umair.almahmood10@gmail.com>
2023-07-09 00:02:23 -0400

    Added more textures

---
+++ src/SettingsButton.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class SettingsButton : TextureButton {
+	ShaderMaterial defaultMaterial;
+	ShaderMaterial hoverMaterial;
+	public override void _Ready() {
+		defaultMaterial = ResourceLoader.Load<ShaderMaterial>("res://assets/shaders/Default.tres");
+		hoverMaterial = ResourceLoader.Load<ShaderMaterial>("res://assets/shaders/Hover.tres");
+		MouseEntered += () => {
+			Material = hoverMaterial;
+		};
+		MouseExited += () => {
+			Material = defaultMaterial;
+		};
+	}
+
+	public override void _Process(double delta) {
+	}
+}
