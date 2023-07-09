d07f10ae50735705c577df22cd5f0d7a8426f8a2
Shadman Syed <shadman3d@gmail.com>
2023-07-09 13:04:05 -0400

    Add info button

--- src/Info.cs
+++ src/Info.cs
@@ -1,15 +1,20 @@
 using Godot;
 using System;
 
-public partial class Info : Control
-{
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+public partial class Info : Control {
+	InfoButton infoButton;
+	bool isHiddin = true;
+	public override void _Ready() {
+		Hide();
+		infoButton = GetNode<InfoButton>("/root/World/CanvasLayer/UISwitcher/InfoButtonWrapper/InfoButton");
+		infoButton.Pressed += () => {
+			if(isHiddin) {
+				Show();
+				isHiddin = false;
+			} else {
+				Hide();
+				isHiddin = true;
+			}
+		};
 	}
 }
